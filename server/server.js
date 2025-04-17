0902546de03f282c392267412fad3642abc411ac
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2025-04-17 14:52:43 +0200

    adding the Login.jsx,Home.jsx,Reset-Password.jsx,EmailVerify.jsx,assets folder,tailwind configurations ,dependancies, setting up the Browser Router,Navbar,Header

--- server/server.js
+++ server/server.js
@@ -3,22 +3,35 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from '../server/config/mongoDB.js';
-import authRouter from '../server/routes/authRouter.js'; 
+import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRoutes.js'; 
+
 
 dotenv.config();
+
 const app = express();
 
+// Set up the server
 const PORT = process.env.PORT || 4000;
+
+// Connect to MongoDB
 connectDB();
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true,}));
-//api endpoints
+app.use(cors({ credentials: true,
+  origin: "http://localhost:5173",}));
+
+//api endpoints 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     });
-app.use('/api/auth', authRouter);
 
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }   );
